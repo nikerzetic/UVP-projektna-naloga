1db8c603e52cd2419a6738227b66c64daefa737f
Nik Erzetic <nikerzetic@gmail.com>
2018-05-19 11:07:10 +0200

    Even more work done on model.py

--- model.py
+++ model.py
@@ -1,9 +1,11 @@
-class YetNameless:
+class App:
 
     def __init__(self, main_file):
         self.boxes = []
+        self.selected = set()
         self.main_file = main_file
         self.refresh_boxes()
+        self.position = 0
 
     def refresh_boxes(self):
         dat = open(self.main_file, 'r', encoding='UTF-8')
@@ -15,14 +17,10 @@ class YetNameless:
         self.save_changes()
 
     def delete_box(self):
-        self.boxes.remove(input('Škatla: ') + '.txt')
+        for box in self.selected:
+            self.boxes.remove(box)
+            self.selected.discard(box)
         self.save_changes()
-
-    def move_note(self, other):
-        source = Box(input('Iz: ') + '.txt')
-        source.delete_note(other)
-        target = Box(input('V: ') + '.txt')
-        target.add_note(other)
 
     def save_changes(self):
         dat = open(self.main_file, 'w', encoding='UTF-8')
@@ -30,12 +28,23 @@ class YetNameless:
             print(box.name, file=dat)
         dat.close()
 
+    def select_box(self):
+        self.selected |= set(self.boxes[self.position])
+
+    def next(self):
+        self.position += 1
+
+    def previous(self):
+        self.position -= 1
+
 
 class Box:
 
     def __init__(self, name):
         self.name = name
         self.content = []
+        self.selected = set()
+        self.position = 0
         self.refresh_notes()
 
     def __repr__(self):
@@ -65,6 +74,26 @@ class Box:
         for note in self.content:
             print(note.text, file=dat)
         dat.close()
+
+    def move_note(self, other):
+        for note in self.selected:
+            other.content.append(note)
+            self.content.remove(note)
+            self.selected.discard(note)
+        self.save_changes()
+        other.save_changes()
+
+    def add_note_to_selection(self):
+        self.selected |= set(self.content[self.position])
+
+    def remove_not_from_selection(self):
+        self.selected.discard(self.content[self.position])
+
+    def next(self):
+        self.position += 1
+
+    def previous(self):
+        self.position -= 1
 
 
 class Note:
