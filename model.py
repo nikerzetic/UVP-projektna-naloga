7063df48c183317ea802851d2eb68b9685eb76b9
Nik Erzetic <nikerzetic@gmail.com>
2018-05-19 16:11:26 +0200

    Working model.py (in theory)

--- model.py
+++ model.py
@@ -1,18 +1,25 @@
 from os import remove
 
+
 class App:
 
-    def __init__(self, main_file):
+    def __init__(self):
         self.boxes = []
         self.selected = set()
-        self.main_file = main_file
+        self.main_file = 'boxes_main_file.txt'
         self.refresh_boxes()
         self.position = 0
 
     def refresh_boxes(self):
-        dat = open(self.main_file, 'r', encoding='UTF-8')
-        self.boxes = [Box(line.strip('\n')) for line in dat]
-        dat.close()
+        try:
+            dat = open(self.main_file, 'r', encoding='UTF-8')
+            self.boxes = [Box(line.strip('\n')) for line in dat]
+            dat.close()
+        except IOError:
+            dat = open(self.main_file, 'w', encoding='UTF-8')
+            print('Praškatla.txt', file=dat)
+            self.boxes.append(Box('Praškatla.txt'))
+            dat.close()
 
     def new_box(self):
         box = Box(input('Škatla: ') + '.txt')
@@ -26,7 +33,7 @@ class App:
             self.boxes.remove(box)
         self.selected = set()
         self.save_changes()
-        self.position -= 1
+        self.position = 0
 
     def save_changes(self):
         dat = open(self.main_file, 'w', encoding='UTF-8')
@@ -37,37 +44,36 @@ class App:
     def select_box(self):
         self.selected.add(self.boxes[self.position])
 
-    def next(self):
+    def next_box(self):
         if self.position != len(self.boxes)-1:
             self.position += 1
         else:
             self.position = 0
 
-    def previous(self):
+    def previous_box(self):
         if self.position != 0:
             self.position -= 1
         else:
             self.position = len(self.boxes) - 1
 
-    def open_box(self):
-        self.boxes[self.position].refresh_notes()
+    def open_current_box(self):
+        self.boxes[self.position].open_box()
 
 
 class Box:
 
     def __init__(self, name):
         self.name = name
+        self.title = self.name.strip('.txt')
         self.content = []
         self.selected = set()
         self.position = 0
 
     def __repr__(self):
-        return 'Box(%s)' % self.name
-
-    def __print__(self):
-        return self.name.strip('.txt')
+        return 'Box("%s")' % self.name
 
     def open_box(self):
+        self.refresh_notes()
         return self.content
 
     def refresh_notes(self):
@@ -78,12 +84,15 @@ class Box:
             dat = open(self.name, 'w', encoding='UTF-8')
         dat.close()
 
-    def add_note(self, other):
-        self.content.append(other.text)
+    def add_note(self):
+        note = Note(input('Zapisek: '))
+        self.content.append(note)
         self.save_changes()
 
-    def delete_note(self, other):
-        self.content.remove(other.text)
+    def delete_note(self):
+        for note in self.selected:
+            self.content.remove(note)
+        self.selected = set()
         self.save_changes()
 
     def save_changes(self):
@@ -101,19 +110,28 @@ class Box:
         other.save_changes()
 
     def add_note_to_selection(self):
-        self.selected |= set(self.content[self.position])
+        self.selected.add(self.content[self.position])
 
     def remove_not_from_selection(self):
         self.selected.discard(self.content[self.position])
 
-    def next(self):
-        self.position += 1
+    def next_note(self):
+        if self.position != len(self.content)-1:
+            self.position += 1
+        else:
+            self.position = 0
 
-    def previous(self):
-        self.position -= 1
+    def previous_note(self):
+        if self.position != 0:
+            self.position -= 1
+        else:
+            self.position = len(self.content) - 1
 
 
 class Note:
 
     def __init__(self, text):
         self.text = text
+
+    def __repr__(self):
+        return 'Note("%s")' % self.text
